da3dcd506954aff26ad910d059e1faf5d7fc943e
Claudia Chin <claudia.chinsh@gmail.com>
2023-06-13 13:24:51 -0700

    reorganise files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Cropper from './lib/Cropper.svelte';
-	import Cropped from './lib/Cropped.svelte';
+	import Cropper from '../lib/Cropper.svelte';
+	import Cropped from '../lib/Cropped.svelte';
 
     // variables to download all the cropped images
     let allCrops = [];
@@ -18,7 +18,7 @@
 </div>
 <!-- App can only be used on tablet size and up. -->
 <div class="main-body" class:hide="{innerWidth < 767}">
-    <Cropper cwidth=500 cheight=750 allCrops={allCrops} bind:filename={filename}/>
+    <Cropper cwidth=500 cheight=750 bind:allCrops={allCrops} bind:filename={filename}/>
     <Cropped bind:allCrops={allCrops} bind:filename={filename}/>
 </div>
 <div class="section main-body" class:hide="{innerWidth > 767}">
