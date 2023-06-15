8bd7a99d1a3dbc542e970a5ccb3da2881673e6d8
Claudia Chin <claudia.chinsh@gmail.com>
2023-06-15 16:26:21 -0700

    update styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,14 @@
     let allCrops = [];
     let filename;
 
+    // colours
+    let colours = {
+        primary: '#ED3996', // cropping lines + fill
+        secondary: '#3B97ED', // axis lines
+        tooltip: '#121212',
+        tooltip_text: '#FFFFFF',
+    }
+
     // media query
     let innerWidth;
 
@@ -13,29 +21,47 @@
 <svelte:window bind:innerWidth={innerWidth}/>
 
 <div class="section header">
-    <h2>Pikrex Cropping Tool</h2>
+    <img src="/favicon.png" alt="logo"/>
 </div>
 <!-- App can only be used on tablet size and up. -->
-<div class="main-body" class:hide="{innerWidth < 767}">
-    <Cropper cwidth=500 cheight=750 bind:allCrops={allCrops} bind:filename={filename}/>
-    <Cropped bind:allCrops={allCrops} bind:filename={filename}/>
+<div class="section main-body" class:hide="{innerWidth < 767}">
+    <Cropper cwidth=500 cheight=750 colours={colours} bind:allCrops={allCrops} bind:filename={filename}/>
+    <Cropped colours={colours} bind:allCrops={allCrops} bind:filename={filename}/>
 </div>
 <div class="section main-body" class:hide="{innerWidth > 767}">
     <p>Seems like the screen is too small!</p>
 </div>
+
 <div class="section footer">
-    <p>Copyright Pikrex Inc. 2023</p>
+    <p>Copyright 2023. Pikrex Inc.</p>
 </div>
 
-<style>
+<style lang="scss">
+    $black: #121212;
+    $dark_grey: #404040;
+    $white: #FFFFFF;
+    $pink: #ED3996;
+    $light_pink: #FAC7E1;
+    $dark_pink: #D51477;
+    $blue: #3B97ED;
+
     .header,
     .footer {
         display: grid;
+        gap: 8px;
         place-items: center;
     }
 
+    .header img {
+        height: 60px;
+    }
+
+    .footer {
+        color: $white;
+    }
+
     .main-body {
-        background: #FFFFFF;
+        background: $white;
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
@@ -46,9 +72,29 @@
         display: none !important;
     }
 
+    // styling the components
     @media screen and (min-width: 1025px) {
         :global(.cropper-wrapper) {
             justify-content: end !important;
         }
+    }
+
+    :global(.upload-button) {
+        background: transparent;
+
+        :global(span) {
+            color: $black;
+        }
+    } 
+
+    :global(.cropped-img-div) {
+        border: solid 2px $blue !important;
+    }
+
+    :global(#save) {
+        border-radius: 1000px;
+        padding: 8px 32px;
+        border: 2px solid $pink;
+        background: transparent;
     }
 </style>
